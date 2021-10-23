faa477eb769dfc77e365f4f19abded0624584b53
caleb-freitas <calebfreitas@tutanota.com>
2021-10-23 14:52:23 -0300

    feat: implement specifications entity

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 app.use(express.json());
 
 // routes
 app.use(categoriesRoutes);
+app.use(specificationsRoutes);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
