286f990a404e3d062c0cb33cb32d24cd02d16241
caleb-freitas <calebfreitas@tutanota.com>
2021-10-25 16:55:22 -0300

    refactor: implement list categories use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,15 @@
 import { Router } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { CreateCategoryUseCase } from '../modules/cars/useCases/createCategory/CreateCategoryUseCase';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/categories', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/categories', (request, response) => {
-  const categories = categoriesRepository.list();
-  return response.json(categories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
