de5f583c9bfa877b1e1ba556e13fc0a7a5fbc94b
caleb-freitas <calebfreitas@tutanota.com>
2021-10-25 18:47:46 -0300

    feat: read the file line by line

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,21 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    // separate the file into chunks
+    const stream = fs.createReadStream(file.path);
+
+    // read documentation
+    const parseFile = csvParse();
+
+    // send the chunck to parseFile
+    stream.pipe(parseFile);
+
+    // read documentation
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
 
