ccee0d5a370150dd19789eb52cf86bbb7c15e596
caleb-freitas <calebfreitas@tutanota.com>
2021-10-25 18:09:58 -0300

    feat: import file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: './temp'
+});
 
 categoriesRoutes.post('/categories', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +15,12 @@ categoriesRoutes.post('/categories', (request, response) => {
 
 categoriesRoutes.get('/categories', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
 
 export { categoriesRoutes };
