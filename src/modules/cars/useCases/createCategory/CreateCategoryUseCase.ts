0bd9538c7e3d1c43c2bcc66f618d8fa95779062a
caleb-freitas <calebfreitas@tutanota.com>
2021-10-25 16:31:57 -0300

    feat: create categories use case (controller)

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,12 +1,12 @@
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+import { CategoriesRepository } from '../../repositories/CategoriesRepository';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
@@ -17,4 +17,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
