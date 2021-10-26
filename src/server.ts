e7a3ea93121286df8f9f8d7785024f280c085e27
caleb-freitas <calebfreitas@tutanota.com>
2021-10-25 21:52:51 -0300

    feat: initial swagger setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
 import { router } from './routes';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 app.use(express.json());
+
+// swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // routes
 app.use(router);
