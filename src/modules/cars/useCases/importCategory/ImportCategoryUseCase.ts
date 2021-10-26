5a89dd4b72a3828c2d0c89993fae1dbcd4ebf000
caleb-freitas <calebfreitas@tutanota.com>
2021-10-25 21:29:17 -0300

    feat: add categories from file to database

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,22 +1,56 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    // separate the file into chunks
-    const stream = fs.createReadStream(file.path);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    // read documentation
-    const parseFile = csvParse();
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
 
-    // send the chunck to parseFile
-    stream.pipe(parseFile);
+      // separate the file into chunks
+      const stream = fs.createReadStream(file.path);
 
-    // read documentation
-    parseFile.on('data', async line => {
-      console.log(line);
+      // read documentation
+      const parseFile = csvParse();
+
+      // send the chunck to parseFile
+      stream.pipe(parseFile);
+
+      // read documentation
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line;
+          categories.push({ name, description });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', err => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    categories.map(async category => {
+      const { name, description } = category;
+      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryAlreadyExists) {
+        this.categoriesRepository.create({
+          name,
+          description
+        });
+      }
     });
   }
 }
-
 export { ImportCategoryUseCase };
