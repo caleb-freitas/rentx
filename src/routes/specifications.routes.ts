2e2dce655063a029d88000c7434fca94dff3d3e4
caleb-freitas <calebfreitas@tutanota.com>
2021-11-03 21:12:52 -0300

    refactor: specifications (dependency injection)

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
+import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post('/specifications', (request, response) => {
-  return createSpecificationController.handle(request, response);
-});
+const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.post(
+  '/specifications',
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
