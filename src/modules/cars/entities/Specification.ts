b623d0c7b92de1abba6e931cccd2beb81c23eefd
caleb-freitas <calebfreitas@tutanota.com>
2021-11-08 20:51:53 -0300

    refactor: add typeorm decorators

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,8 +1,15 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
+@Entity('specifications')
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @CreateDateColumn()
   description: string;
   created_at: Date;
 
