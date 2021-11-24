e59f79a85fbc803a676d250b7b16ba2efd2cd2db
caleb-freitas <calebfreitas@tutanota.com>
2021-11-24 18:46:12 -0300

    refactor: add user to the container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,11 @@ import { container } from 'tsyringe';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
+import { UserRepository } from '../../modules/accounts/repositories/implemetations/UsersRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -15,3 +18,5 @@ container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository
 );
+
+container.registerSingleton<IUsersRepository>('UserRepository', UserRepository);
