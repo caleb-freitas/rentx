9faf6cd4af7f0f4960a1f19e001a9f52fda3b0d9
caleb-freitas <calebfreitas@tutanota.com>
2022-01-10 15:30:40 -0300

    fix: add database host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (): Promise<Connection> => {
+export default async (host = 'database'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
+      host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentx_integration_tests'
