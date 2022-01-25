fa4128d87c3b6861dbe5d33d24c00e7b793ee7bf
caleb-freitas <calebfreitas@tutanota.com>
2022-01-24 22:49:09 -0300

    feat: change from 1m to 15m expires token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: '21d5dff97f925a602197498996c4e945',
-  expires_in_token: '1m',
+  expires_in_token: '15m',
   secret_refresh_token: 'a95c530a7af5f492a74499e70578d150',
   expires_refresh_token: '30d',
   expires_refresh_token_days: 30
